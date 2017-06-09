37cca1e4dc4da0b4cf07612f8eec7ab49d1f00b3
kentanvictor <2030457926@qq.com>
2017-06-09 17:50:00 +0800

    活動和服務進行通信

--- app/src/main/java/com/example/dell/servicetest/MyService.java
+++ app/src/main/java/com/example/dell/servicetest/MyService.java
@@ -2,16 +2,30 @@ package com.example.dell.servicetest;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
 public class MyService extends Service {
+    private DownloadBinder mBinder = new DownloadBinder();
+    class DownloadBinder extends Binder
+    {
+        public void startDownload()
+        {
+            Log.d("MyService","startDownload executed");
+        }
+        public int getProgress()
+        {
+            Log.d("MyService","getProgress executed");
+            return 0;
+        }
+    }
     public MyService() {
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        return mBinder;
     }
 
     @Override
