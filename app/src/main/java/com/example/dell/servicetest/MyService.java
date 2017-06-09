671951ec38e8f5ae7cb10ef2bc29013dcfe8deec
kentanvictor <2030457926@qq.com>
2017-06-09 21:19:54 +0800

    前台服務

--- app/src/main/java/com/example/dell/servicetest/MyService.java
+++ app/src/main/java/com/example/dell/servicetest/MyService.java
@@ -1,9 +1,13 @@
 package com.example.dell.servicetest;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Binder;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 public class MyService extends Service {
@@ -33,6 +37,17 @@ public class MyService extends Service {
         super.onCreate();
         Log.d("MyService","onCreate executed");
         //只在服務第一次創建的時候被調用
+        Intent intent = new Intent(this,MainActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(this,0, intent, 0);
+        Notification notification = new NotificationCompat.Builder(this)
+                .setContentTitle("This is content title")
+                .setContentText("This is content text")
+                .setWhen(System.currentTimeMillis())
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher))
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentIntent(pi)
+                .build();
+        startForeground(1,notification);
     }
 
     @Override
