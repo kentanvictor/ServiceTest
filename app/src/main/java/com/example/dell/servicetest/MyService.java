6152374e209e85e9d03fbb0f44cea10e34ce2453
kentanvictor <2030457926@qq.com>
2017-06-09 17:41:12 +0800

    服務的基本用法(測試)

--- app/src/main/java/com/example/dell/servicetest/MyService.java
+++ app/src/main/java/com/example/dell/servicetest/MyService.java
@@ -18,12 +18,14 @@ public class MyService extends Service {
     public void onCreate() {
         super.onCreate();
         Log.d("MyService","onCreate executed");
+        //只在服務第一次創建的時候被調用
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d("MyService","onStartCommend executed");
         return super.onStartCommand(intent, flags, startId);
+        //每次服務啟動的時候被調用
     }
 
     @Override
