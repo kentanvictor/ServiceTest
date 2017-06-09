07d82a02b47af0e85875e9444f436b749bed04f8
kentanvictor <2030457926@qq.com>
2017-06-09 10:43:48 +0800

    second commit

--- app/src/main/java/com/example/dell/servicetest/MyService.java
+++ app/src/main/java/com/example/dell/servicetest/MyService.java
@@ -12,4 +12,19 @@ public class MyService extends Service {
     public IBinder onBind(Intent intent) {
         throw new UnsupportedOperationException("Not yet implemented");
     }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
 }
